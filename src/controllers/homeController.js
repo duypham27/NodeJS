db3528fd9fbe39d819947bb9ff6e8993b02b83c9
duy_pham <phambachanduy@gmail.com>
2025-03-01 11:07:23 +0700

    update data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,4 @@
+import { query } from 'express';
 import db from '../models/index';
 import CRUDService from '../services/CRUDService';
 
@@ -30,12 +31,45 @@ let postCRUD = async (req, res) => {
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
 
-    console.log('-----------------------------------------------');
+    console.log('-------------------------');
     console.log(data);
-    console.log('-----------------------------------------------');
+    console.log('-------------------------');
 
     return res.render('displayCRUD.ejs', {
         dataTable: data
+    });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+
+        // console.log('-------------------------');
+        // console.log(userData);
+        // console.log('-------------------------');
+
+        // let userData
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('Users not found !');
+    }
+
+}
+
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
     });
 }
 
@@ -45,4 +79,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
