4532b6ef2a4535d04f667f3d9d12b6d8619a7417
duy_pham <phambachanduy@gmail.com>
2025-03-04 15:33:35 +0700

    api login

---
+++ src/services/userService.js
@@ -0,0 +1,80 @@
+import db from "../models/index";
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                //user already exist
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+                    // attributes: {
+                    //     include: ['email', 'roleId'],
+                    //     exclude: ['password'],
+                    // }
+                });
+                if (user) {
+                    //compare password 
+                    let check = await bcrypt.compare(password, user.password);
+
+                    // result == false
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User's not found`;
+
+                }
+
+            } else {
+                //return error
+                userData.errCode = 1;
+                userData.errMessage = `Your's email isn't exist in our systerm. Please try other email !`;
+            }
+
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+
+        } catch (e) {
+            reject(e);
+        }
+
+    })
+}
+
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
