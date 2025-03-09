13aa711e35ddd51f43eb1223e493c7f137e369cc
Pham Bach An Duy <phambachanduy@gmail.com>
2025-03-09 12:17:49 +0700

    allow cors

--- src/server.js
+++ src/server.js
@@ -3,24 +3,35 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import connectDB from "./config/connectDB";
+import cors from "cors";
 
-require('dotenv').config();
+require("dotenv").config();
 
 let app = express();
 
-//config app
+// Cấu hình CORS chính xác
+app.use(cors({
+    origin: "http://localhost:3000", // Chỉ cho phép frontend chạy trên cổng 3000 truy cập
+    credentials: true,  // Cho phép gửi cookie & token
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization"
+}));
+
+// Middleware xử lý dữ liệu từ client
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Cấu hình view engine
 viewEngine(app);
+
+// Khởi tạo routes
 initWebRoutes(app);
 
+// Kết nối database
 connectDB();
 
+// Khởi động server
 let port = process.env.PORT || 6969;
-//PORT === undefined => port = 6969
-
 app.listen(port, () => {
-    //call back
-    console.log("Backend NodeJS is running on the PORT: " + port);
+    console.log("✅ Backend NodeJS is running on PORT: " + port);
 });
