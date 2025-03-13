79597ef28bc3258181c629a655f7bd0b32fb7879
Pham Bach An Duy <phambachanduy@gmail.com>
2025-03-13 12:47:34 +0700

    add api route

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,22 @@
+import { where } from "sequelize";
 import db from "../models/index";
 import bcrypt from 'bcrypt';
 const saltRounds = 10;
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            let salt = await bcrypt.genSalt(saltRounds);
+            let hashPassword = await bcrypt.hash(password, salt);
+            resolve(hashPassword);
+
+        } catch (e) {
+            reject(e);
+        }
+
+    })
+}
 
 let handleUserLogin = (email, password) => {
     return new Promise(async (resolve, reject) => {
@@ -54,7 +70,6 @@ let handleUserLogin = (email, password) => {
     })
 }
 
-
 let checkUserEmail = (userEmail) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -102,7 +117,116 @@ let getAllUsers = (userId) => {
     })
 }
 
+let createNewUser = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //check email is exist ?
+            let check = await checkUserEmail(data.email);
+            if (check === true) {
+                resolve({
+                    errCode: 1,
+                    message: 'Your email is already exist, Plz try another email!'
+                });
+            }
+
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phoneNumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId
+            })
+
+            resolve({
+                errCode: 0,
+                message: 'OK'
+            });
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let deleteUser = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                // raw: false
+            });
+
+            if (!user) {
+                resolve({
+                    errCode: 2,
+                    errMessage: `The user isn't exist`
+                });
+            }
+
+            await db.User.destroy({
+                where: { id: userId }
+            });
+
+            resolve({
+                errCode: 0,
+                message: `The user is deleted`
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.id) {
+                resolve({
+                    errCode: 2,
+                    errMessage: 'Missing required parameters'
+                })
+            }
+            let user = await db.User.findOne({
+                where: { id: data.id },
+                raw: false
+            })
+
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+
+                await user.save();
+                // await db.User.save({
+                //     firstName: data.firstName,
+                //     lastName: data.lastName,
+                //     address: data.address
+                // }, { where: { id: userId } })
+
+                resolve({
+                    errCode: 0,
+                    message: 'Update user success!'
+
+                })
+            } else {
+                resolve({
+                    errCode: 1,
+                    errMessage: `User's not found!`
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUsers: getAllUsers,
+    createNewUser: createNewUser,
+    updateUserData: updateUserData,
+    deleteUser: deleteUser
 }
