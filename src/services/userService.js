8e78b937648a1dd01cc12c5afec0449cffe0b136
Pham Bach An Duy <phambachanduy@gmail.com>
2025-05-06 14:00:36 +0700

    [UPDATE] Add more data User & fix datatypes gender

--- src/services/userService.js
+++ src/services/userService.js
@@ -135,9 +135,10 @@ let createNewUser = (data) => {
                     firstName: data.firstName,
                     lastName: data.lastName,
                     address: data.address,
-                    phonenumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
-                    roleId: data.roleId
+                    phonenumber: data.phonenumber,
+                    gender: data.gender,
+                    roleId: data.roleId,
+                    positionId: data.positionId
                 })
 
                 resolve({
