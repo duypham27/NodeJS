2d8e5c6a6ae3bccc3bb596d8f299b766ab19708e
Pham Bach An Duy <phambachanduy@gmail.com>
2025-06-14 21:55:40 +0700

    [UPDATE] Sequelize Relationship User hasOne - Markdown belongTo && api getDetailDoctorById

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -45,8 +45,24 @@ let postInfoDoctors = async (req, res) => {
     }
 }
 
+let getDetailDoctorById = async (req, res) => {
+    try {
+
+        let info = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(info);
+
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...',
+        });
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    postInfoDoctors: postInfoDoctors
+    postInfoDoctors: postInfoDoctors,
+    getDetailDoctorById: getDetailDoctorById,
 };
