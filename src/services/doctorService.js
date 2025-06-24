cfdc923026ad9daab3e98c43312c94ce6c595fbe
Pham Bach An Duy <phambachanduy@gmail.com>
2025-06-24 19:46:06 +0700

    [UPDATE] Add relation model schedule&allcode and autoload schedule doctor date

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -166,17 +166,9 @@ let bulkCreateSchedule = (data) => {
                     },
                 );
 
-                //convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
-
                 //compare diffenrent
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 //create data
@@ -209,7 +201,12 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date,
-                    }
+                    },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true,
                 })
 
                 if (!dataSchedule) dataSchedule = [];
