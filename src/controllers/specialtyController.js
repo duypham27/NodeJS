55488be75486296b2cbe43e17c6f4bcbb61fcb83
Pham Bach An Duy <phambachanduy@gmail.com>
2025-07-06 16:44:46 +0700

    [UPDATE] API for getAllSpecialty to loading data

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -14,6 +14,21 @@ let createSpecialty = async (req, res) => {
     }
 }
 
+let getAllSpecialty = async (req, res) => {
+    try {
+        let info = await specialtyService.getAllSpecialty();
+        return res.status(200).json(info);
+
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...',
+        });
+    }
+}
+
 module.exports = {
-    createSpecialty: createSpecialty
+    createSpecialty: createSpecialty,
+    getAllSpecialty: getAllSpecialty
 }
