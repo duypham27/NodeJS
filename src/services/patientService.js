aa20af2876e0e04237eb6cf4e4b3a6984ebb7131
Pham Bach An Duy <phambachanduy@gmail.com>
2025-07-15 11:56:15 +0700

    [UPDATE] Fix bug booking function && get api timeTypeData

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -12,7 +12,7 @@ let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
             if (!data.email || !data.doctorId || !data.timeType || !data.date
-                || !data.fullName || !data.seletedGender || !data.address
+                || !data.fullName || !data.selectedGender || !data.address
             ) {
 
                 resolve({
@@ -37,9 +37,9 @@ let postBookAppointment = (data) => {
                     defaults: {
                         email: data.email,
                         roleId: 'R3',
-                        gender: data.seletedGender,
+                        gender: data.selectedGender,
                         address: data.address,
-                        firtName: data.fullName,
+                        firstName: data.fullName,
                     },
                 });
 
